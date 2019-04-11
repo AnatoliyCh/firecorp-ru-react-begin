3f6b2554d89dabad6e0cbefea7f802d817216ede
AnatoliyCH <googlsocial97@gmail.com>
2019-04-11 23:40:01 +0700

    Начат администратор

---
+++ src/app/components/Administrator/index.js
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from 'react';
+
+class Administrator extends Component {
+
+    render() {
+        return (
+            <div>Hello, Administrator</div>
+        )
+    }
+}
+
+export default Administrator;
