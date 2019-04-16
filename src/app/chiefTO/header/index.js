95eb28a5eaafb1fd4a960b2a104bdc927579fd9b
pitchycherry <nik_kirienko@mail.ru>
2019-04-16 16:13:24 +0700

    Correct header

--- src/app/chiefTO/header/index.js
+++ src/app/chiefTO/header/index.js
@@ -3,34 +3,37 @@ import {Link} from "react-router-dom";
 
 const Header = ({match}) => (
     <Fragment>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-xl navbar-light bg-light">
             <Link to={`${match.url}/locations`} className="navbar-brand">
                 <img src={require("./img/logo.jpg")} width="30" height="30"
                      className="d-inline-block align-top mr-2" alt=""/>
                 Огнезащитная корпорация</Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"> </span>
+            </button>
             <div className="collapse navbar-collapse" id="navbarNav">
+                <div className="navbar-nav mr-auto">
+                    <Link to={`${match.url}/notifications`} className="nav-item nav-link">Уведомления</Link>
+                    <Link to={`${match.url}/locations`} className="nav-item nav-link">Локации</Link>
+                    <Link to={`${match.url}/technicians`} className="nav-item nav-link">Техники</Link>
+                    <Link to={`${match.url}/monthlyto`} className="nav-item nav-link">Ежемесячное ТО</Link>
+                    <Link to={`${match.url}/objects`} className="nav-item nav-link">Объекты</Link>
+                    <Link to={`${match.url}/calendar`} className="nav-item nav-link">Календарь</Link>
+                    <Link to={`${match.url}/map`} className="nav-item nav-link">Карта</Link>
+                </div>
                 <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <Link to={`${match.url}/notifications`} className="nav-link" href="#">Уведомления</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to={`${match.url}/locations`} className="nav-link" href="#">Локации</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to={`${match.url}/technicians`} className="nav-link" href="#">Техники</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to={`${match.url}/monthlyto`} className="nav-link" href="#">Ежемесячное ТО</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to={`${match.url}/objects`} className="nav-link" href="#">Объекты</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to={`${match.url}/calendar`} className="nav-link" href="#">Календарь</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to={`${match.url}/map`} className="nav-link" href="#">Карта</Link>
-                    </li>
+                    <div className="nav-item">
+                        <div className="row">
+                            <div className="header-profile_text text-right">
+                                <p>Начальник ТО</p>
+                                <p>Филлипенко К.Е.</p>
+                            </div>
+                            <img src={require("./img/logo.jpg")} className="ml-2 mr-2 round-img" width="50" height="50"
+                                 alt=""/>
+                        </div>
+
+                    </div>
                 </ul>
             </div>
         </nav>
