ce31aa3002aee793645ea47e5d3c1da26a5aa988
AnatoliyCH <googlsocial97@gmail.com>
2019-04-21 19:51:16 +0700

    начат администратор : users

--- src/app/Administrator/Users/index.js
+++ src/app/Administrator/Users/index.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
+import './styles.css';
+
 class Users extends Component {
 
     render() {
         return (
-            <div>Hello, Users</div>
+            <div className="card hh">
+                <div className="card-header">
+                    Featured
+                </div>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">Cras justo odio</li>
+                    <li className="list-group-item">Dapibus ac facilisis in</li>
+                    <li className="list-group-item">Vestibulum at eros</li>
+                </ul>
+            </div>
         )
     }
 }
