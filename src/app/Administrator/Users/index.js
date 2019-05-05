656497bce65dbdf5a75c2b0b40860bad920154f7
AnatoliyCH <googlsocial97@gmail.com>
2019-05-05 21:45:24 +0700

    users 90%

--- src/app/Administrator/Users/index.js
+++ src/app/Administrator/Users/index.js
@@ -35,18 +35,7 @@ class Users extends Component {
             let usersNew = [];//отсортированные и обработанные пользователи
             data.forEach(function (itemData) {
                 //сортировка и обработка
-                // eslint-disable-next-line
-                if (i == itemData.typeId) {
-                    usersNew.push({
-                        login: itemData.account.login,
-                        password: itemData.account.password,
-                        loginPhone: itemData.account.loginPhone.value,
-                        firstName: itemData.firstName,
-                        middleName: itemData.middleName,
-                        lastName: itemData.lastName,
-                        typeId: itemData.typeId,
-                    });
-                }
+                if (i === itemData.typeId) usersNew.push(itemData);
             });
             usersLists.push({title: itemMap, data: usersNew});
             usersNew = [];
