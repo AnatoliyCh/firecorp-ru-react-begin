be79e5f6aa4a689a3c7a561f8f3b68253d1feff7
AnatoliyCH <googlsocial97@gmail.com>
2019-05-21 18:52:17 +0700

    Начал Streets

--- src/app/Administrator/catalog/Street/index.js
+++ src/app/Administrator/catalog/Street/index.js
@@ -1,5 +1,13 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import * as allConst from "../../../commonComponents/Const";
+import '../styles.css';
+import SpinnerDanger from '../../../commonComponents/Loading/BootstrapBorderSpinnerDangerDefault';
+
+let typeStreet = new Map([
+    [16, "Улица"],
+    [32, "Проспект"],
+    [48, "Шоссе"],
+]);
 
 class Street extends Component {
     state = {
@@ -18,14 +26,40 @@ class Street extends Component {
         }).then(function (response) {
             return response.json();
         }).then(data => {
-            console.log(data);
+            this.sortData(data);
         }).catch((error) => {
             console.log(error.message);
         });
     };
 
+    sortData = (data) => {
+        let streets = [];//sort список улиц
+        data.forEach(function (itemData, iData) {
+            streets.push({ name: itemData.name, typeStr: typeStreet.get(itemData.type), typeInt: itemData.type })
+        });
+        console.log(data);
+        streets.sort((a, b) => {
+            if (a.name.toLowerCase() < b.name.toLowerCase())  return -1;
+            if (a.name.toLowerCase() > b.name.toLowerCase()) return 1;
+            return 0;
+        });
+        console.log(streets);
+        this.setState({isLoading: false});
+    };
+
     render() {
-        return null;
+        return (
+            <Fragment>
+                {
+                    this.state.isLoading ? <div className="catalogSpinner"><SpinnerDanger/></div>
+                        :
+                        <Fragment>
+
+                        </Fragment>
+                }
+            </Fragment>
+        )
     }
 }
+
 export default Street;
